f4af6befe33cbaa046db03722a5140062075e4fd
Purpurum <mollllest@gmail.com>
2023-10-28 08:32:06 +0300

    modified:   .gitignore 	modified:   detectors.py 	new file:   models/YOLOv8/video0_n.pt 	renamed:    models/nano8_gc74.pt -> models/YOLOv8/yolov8_combined_pre_s.pt 	renamed:    models/small5_pre.pt -> models/YOLOv8/yolov8_combined_s.pt 	deleted:    models/nano8_thesis90.pt 	deleted:    models/small8_60.pt 	new file:   ngrok.exe 	modified:   ui.py

--- detectors.py
+++ detectors.py
@@ -3,38 +3,27 @@ import yolov5
 from ultralytics import YOLO
 from PIL import Image
 
-def load_detector_model(modelname):
+def load_detector_model(model_path, model_arch = "YOLOv8"):
     #TODO
     #Ифы на каждую модель через аргумент, лишние по итогу убрать
     #Функция возвращает загруженную модель
-    if modelname == "YOLOv5":
-        yolov5_models_folder = "models/yolo/v5/"
-        yolov5_models = [os.path.join(yolov5_models_folder, f) for f in os.listdir(yolov5_models_folder) if os.path.isfile(os.path.join(yolov5_models_folder, f))]
-        model_yolov5 = yolov5.load(yolov5_models[0])
-        return model_yolov5
 
-    if modelname == "YOLOv8":
-        yolov8_models_folder = "models/yolo/v8/"
-        yolov8_models = [os.path.join(yolov8_models_folder, f) for f in os.listdir(yolov8_models_folder) if os.path.isfile(os.path.join(yolov8_models_folder, f))]
-        model_yolov8 = YOLO(yolov8_models[0])
+    if model_arch == "YOLOv8":
+        model_yolov8 = YOLO(model_path)
         return model_yolov8
 
-def detect(modelname, model, image):
+def detect(model, image, modelname = "YOLOv8"):
     #TODO
     #Ифы на каждую модель через аргумент, лишние по итогу убрать
     #Функция возвращает загруженную модель
     if modelname == "YOLOv8":
+        
         results = model(image)
         im_array = results[0].plot()
-        img_with_boxes = Image.fromarray(im_array)
+        img_with_boxes = Image.fromarray(im_array[..., ::-1])
+        try:
+            img_with_boxes.save("results.png")
+        except:
+            pass
         return img_with_boxes, results
     
-    if modelname == "YOLOv5":
-        results = model(image, size=640)
-        predictions = results.pred[0]
-        boxes = predictions[:, :4] # x1, y1, x2, y2
-        scores = predictions[:, 4]
-        categories = predictions[:, 5]
-        r_img = results.render()
-        img_with_boxes = r_img[0]
-        return img_with_boxes, results
--- ui.py
+++ ui.py
@@ -3,7 +3,6 @@ import os
 import cv2
 import numpy as np
 import streamlit as st
-from camera_input_live import camera_input_live
 from PIL import Image
 import time
 import tempfile
@@ -15,90 +14,90 @@ from utils import get_files_in_directory
 
 
 def main():
-    selector_option = st.sidebar.selectbox("Выберите модель детекции", options=[1,2,3]) #Она выбирается в мейне ЫЛЬЯ!
+    yolov8_model_paths, yolov8_model_names = get_files_in_directory("models/YOLOv8")
+    selector_option = st.sidebar.selectbox("Выберите модель детекции", options=yolov8_model_names) #Она выбирается в мейне ЫЛЬЯ!
     st.sidebar.header('Ввод') 
     st.text(body=selector_option) #Она выбирается в мейне ЫЛЬЯ!
-    selected_model = load_detector_model(selector_option) #Она выбирается в мейне ЫЛЬЯ!
+    selected_model = load_detector_model(yolov8_model_paths[yolov8_model_names.index(selector_option)]) #Она выбирается в мейне ЫЛЬЯ!
+    
+    print(selected_model)
     st.title('Вывод')
-    load_camera()
-        
+  
+    def load():
+        load_type = ['jpg','png','jpeg','zip','rar','mkv','mp4','mpg','mpeg','mpeg4']
+        uploaded_data= st.sidebar.file_uploader(label="Выберете файл для распознования",type = load_type)
+        if uploaded_data is not None:
+            file_type = get_file_type(uploaded_data.name)
+            print(file_type)
+            if any(file_type == i for i in load_type[:3]):load_img(uploaded_data)
+            elif any(file_type == i for i in load_type[3:5]):load_zip(uploaded_data)
+            elif any(file_type == i for i in load_type[5:]):load_video(uploaded_data)
+            else:st.text("Ошибка чтения файла, возможно неподходящий формат\n(убедитесь что в названии файла нет точек и специальных символов)")          
+        else:
+            return None
+    
+
+    def get_file_type(file):
+        return os.path.splitext(file)[1][1:]
+
+    def load_img(img_file):
+        img = img_file.getvalue()
+        st.image(img)
+        image = Image.open(BytesIO(img))
+        image, results = detect(selected_model, image)
+        st.image(image)
+        st.text(results)
+
+    def load_zip(zip_file):
+        results_list = []
+        with tempfile.TemporaryDirectory() as temp_dir:
+            with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+                zip_ref.extractall(temp_dir)      
+            for filename in os.listdir(temp_dir):
+                file_path = os.path.join(temp_dir, filename)
+                image, result = detect(selector_option, selected_model,file_path)
+                results_list.append(result)
+
+        with open('results.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(results_list)
+
+    def load_video(video_file):
+        tfile = tempfile.NamedTemporaryFile(delete=False) 
+        tfile.write(video_file.read())
+        video_capture = cv2.VideoCapture(tfile.name)
+        FRAME_WINDOW = st.image([])
+
+        while True:
+            ret, frame = video_capture.read()
+            frame = Image.fromarray(frame[..., ::-1])
+            if not ret:
+                break 
+            
+            processed_frame, results = detect(selected_model, frame)
+            FRAME_WINDOW.image(processed_frame)
+    
+    def load_camera():
+        run = st.sidebar.checkbox('Run')
+        if run:    
+            FRAME_WINDOW = st.image([])
+            RESULTS_WIDNOW = st.image([])
+            camera = cv2.VideoCapture(0)
+
+        while run:
+            _, frame = camera.read()
+            frame = Image.fromarray(frame[..., ::-1])
+            image, results = detect(selected_model, frame)
+            FRAME_WINDOW.image(frame)
+            RESULTS_WIDNOW.image(image)
+
+        else:
+            st.write('Stopped')
+    
+    load_camera() 
     load()
-
-def load():
-    load_type = ['jpg','png','jpeg','zip','rar','mkv','mp4','mpg','mpeg','mpeg4']
-    uploaded_data= st.sidebar.file_uploader(label="Выберете файл для распознования",type = load_type)
-    if uploaded_data is not None:
-        file_type = get_file_type(uploaded_data.name)
-        print(file_type)
-        if any(file_type == i for i in load_type[:3]):load_img(uploaded_data)
-        elif any(file_type == i for i in load_type[3:5]):load_zip(uploaded_data)
-        elif any(file_type == i for i in load_type[5:]):load_video(uploaded_data)
-        else:st.text("Ошибка чтения файла, возможно неподходящий формат\n(убедитесь что в названии файла нет точек и специальных символов)")          
-    else:
-        return None
-    
-
-def get_file_type(file):
-    return os.path.splitext(file)[1][1:]
-
-def load_img(img_file):
-    img = img_file.getvalue()
-    st.image(img)
-    image = Image.open(BytesIO(img))
-    image = np.asarray(image)
-    image, results = detect("YOLOv8", selected_model, image)
-    st.image(image)
-    st.text(results)
-
-def load_zip(zip_file):
-    results_list = []
-
-    with tempfile.TemporaryDirectory() as temp_dir:
-        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-            zip_ref.extractall(temp_dir)
-        
-        for filename in os.listdir(temp_dir):
-            file_path = os.path.join(temp_dir, filename)
-            image, result = detect("YOLOv8",selected_model,file_path)
-            results_list.append(result)
-
-    with open('results.csv', 'a', newline='') as csvfile:
-      writer = csv.writer(csvfile)
-      writer.writerows(results_list)
-
-def load_video(video_file):
-    tfile = tempfile.NamedTemporaryFile(delete=False) 
-    tfile.write(video_file.read())
-    video_capture = cv2.VideoCapture(tfile.name)
-    FRAME_WINDOW = st.image([])
-
-    while True:
-        ret, frame = video_capture.read()
-        if not ret:
-            break 
-        
-        processed_frame, results = detect("YOLOv8", selected_model, frame)
-        FRAME_WINDOW.image(processed_frame)
-    
-def load_camera():
-    run = st.sidebar.checkbox('Run')
-    if run:    
-      FRAME_WINDOW = st.image([])
-      RESULTS_WIDNOW = st.image([])
-      camera = cv2.VideoCapture(0)
-
-      while run:
-          _, frame = camera.read()
-          frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-          image, results = detect("YOLOv8", selected_model, frame)
-          FRAME_WINDOW.image(frame)
-          RESULTS_WIDNOW.image(image)
-
-      else:
-          st.write('Stopped')
     
 
 
 if __name__ == '__main__':
-    selected_model = None #Она выбирается в мейне ЫЛЬЯ!
     main()
